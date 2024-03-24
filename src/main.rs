e4641f3be333d947a2865bf9de8158af9925d2a1
rafaeelricco <rafaelricco10@gmail.com>
2024-03-24 16:26:39 -0300

    feat(api-infos): added api infos to root route

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ async fn main() -> std::io::Result<()> {
     let mut client_options = ClientOptions::parse(db_url).await.expect("Failed to connect to the server");
     client_options.app_name = Some("RustCurd".to_string());
     let client = Client::with_options(client_options).expect("Failed to create client");
+    println!("Connected to the database");
     let db = client.database("RUSTcurd");
     create_title_unique(&client).await;
     println!("Server is running at {}", address);
--- src/start_server.rs
+++ src/start_server.rs
@@ -1,13 +1,45 @@
-use actix_web::{web, App, HttpServer};
-use mongodb::{Database};
+use crate::routes::*;
+
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use mongodb::Database;
 use std::net::TcpListener;
 use actix_web::dev::Server;
-use crate::routes::*;
+use serde::Serialize;
+use chrono::Utc;
+
+#[derive(Serialize)]
+struct ApiInfo {
+    api: &'static str,
+    version: &'static str,
+    database: Option<String>,
+    date_created: String
+}
+
+pub async fn root() -> impl Responder {
+
+    const API_VERSION: &str = env!("CARGO_PKG_VERSION");
+    const API_NAME: &str = env!("CARGO_PKG_NAME");
+    
+    let date_created: String = Utc::now().format("%d-%m-%Y").to_string();
+
+
+    let api_infos: ApiInfo = ApiInfo {
+        api: API_NAME,
+        version: API_VERSION,
+        date_created: date_created,
+        database: None
+    };
+
+    HttpResponse::Ok().json(api_infos)
+
+}
+
 
 pub fn run(listener: TcpListener, db: Database) -> Result<Server, std::io::Error> {
     let db = web::Data::new(db);
     let server = HttpServer::new(move || {
         App::new()
+            .route("/", web::get().to(root)) // Adicione esta linha para a rota raiz
             .route("/createNote", web::post().to(create_note_route))
             // Adicione as rotas para read_note, update_note e delete_note aqui.
             .app_data(db.clone())
